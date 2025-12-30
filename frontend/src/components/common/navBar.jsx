9c871d21b71d4c37e66975b7ae9bb5b3f47fc6e8
g-edgah <172597903+g-edgah@users.noreply.github.com>
2025-12-30 16:31:18 +0400

    nav  bar

--- frontend/src/components/common/navBar.jsx
+++ frontend/src/components/common/navBar.jsx
@@ -1,6 +1,8 @@
 import { Link } from 'react-router-dom';
 import { PiFlowerLotusLight } from "react-icons/pi";
 import { HiUser } from "react-icons/hi2";
+import { ImSearch } from "react-icons/im";
+import { HiMiniXMark } from "react-icons/hi2";
 import { IoIosBasket } from "react-icons/io";
 import { BiMenu } from "react-icons/bi";
 import { useState } from 'react';
@@ -12,8 +14,8 @@ const NavBar = () => {
     
     return (
         <>
-            <div className="nav w-screen flex flex-col justify-around pt-2 pb-1 md:pt-3 h-23 md:h-25">
-                <div className="flex items-center justify-between pl-2 pr-5.5 xs:pr-7 xs:pl-4 md:px-10 lg:px-25 w-full md:w-full">
+            <div className={`nav w-screen flex flex-col justify-around pt-2 pb-1 md:pt-3 bg-gray-200 relative ${isSearch ? 'h-30 md:h-30' : 'h-23 md:h-23'}`} >
+                <div className="flex items-center justify-between pl-2 pr-5.5 xs:pr-7 xs:pl-4 md:px-10 lg:px-25 w-full md:w-full absolute top-1 md:top-7 h-10">
                     {/* left */}
                     <div className='flex items-center space-x-4 '>
                         <button className='md:hidden'>
@@ -27,24 +29,36 @@ const NavBar = () => {
                     </div>
 
                     {/* center */}
-                    {(!isSearch)&& (<div className="hidden md:flex space-x-6">
-                        <Link className="text-700 hover:text-nav-hover text-sm font-medium" >bouquets</Link>
-                        <Link className="text-700 hover:text-nav-hover text-sm font-medium" >flowers</Link>
-                        <Link className="text-700 hover:text-nav-hover text-sm font-medium" >occassions</Link>
-                        <Link className="text-700 hover:text-nav-hover text-sm font-medium" >colors</Link>
-                        <Link className="text-700 hover:text-nav-hover text-sm font-medium" >popular</Link>
-                    </div>)}
+                    {(!isSearch) ? (<div className="hidden md:flex space-x-6 font-medium text-[15px]">
+                        <Link className="text-700 hover:text-nav-hover" >bouquets</Link>
+                        <Link className="text-700 hover:text-nav-hover" >flowers</Link>
+                        <Link className="text-700 hover:text-nav-hover" >occassions</Link>
+                        <Link className="text-700 hover:text-nav-hover" >colors</Link>
+                        <Link className="text-700 hover:text-nav-hover" >popular</Link>
+                    </div>) : (
+                        <div className="hidden md:flex justify-center w-300">
+                            <SearchBar isSearch={isSearch} setIsSearch={setIsSearch}/>
+                        </div>
+                    )}
                     
 
                     {/* right */}
                     <div className="flex items-center space-x-4">
-                        <div className={`${isSearch ? 'hidden md:flex' : 'flex'}`}>
-                            <SearchBar isSearch={isSearch} setIsSearch={setIsSearch}/>
+                        <div className={`hover:text-nav-hover ${isSearch ? 'hidden md:flex' : 'flex'}`}>
+                            {isSearch ? (
+                                <button onClick={()=> setIsSearch(!isSearch)}>
+                                    <HiMiniXMark className="h-6 w-6"/>
+                                </button>
+                            ) : (
+                                <button onClick={()=> setIsSearch(!isSearch)}>
+                                    <ImSearch className="h-5 w-5"/>
+                                </button>
+                            )}
                         </div>
                         <Link to='/profile' className="hover:text-nav-hover">
                             <HiUser className="h-6 w-6 text-black-700"/>
                         </Link>
-                        <button className="relative hover:text-nav-hover">
+                        <button className="relative hover:text-nav-hover ">
                             <IoIosBasket className="h-6 w-6 text-black-700"/>
                             <span className="absolute -top-1 bg-topbar text-white text-xs rounded-full px-2 py-0.5">0</span>
                             
@@ -52,18 +66,18 @@ const NavBar = () => {
                         
                     </div>  
                 </div>
-                <div className={`${isSearch ? 'flex md:hidden justify-center mt-2' : 'hidden'}`}>
+                <div className={`${isSearch ? 'flex md:hidden justify-center mt-2 absolute top-9 md:top-2/4 w-full' : 'hidden'}`}>
                     <SearchBar isSearch={isSearch} setIsSearch={setIsSearch}/>
                 </div>
                     
                 
 
-                <div className={`flex justify-around px-1  xs:space-x-6 xs:items-center xs:justify-center ${isSearch ? 'flex' : 'md:hidden'}`}>
-                        <Link className="text-700 hover:text-nav-hover text-sm font-medium" >bouquets</Link>
-                        <Link className="text-700 hover:text-nav-hover text-sm font-medium" >flowers</Link>
-                        <Link className="text-700 hover:text-nav-hover text-sm font-medium" >occassions</Link>
-                        <Link className="text-700 hover:text-nav-hover text-sm font-medium" >colors</Link>
-                        <Link className="text-700 hover:text-nav-hover text-sm font-medium" >popular</Link>
+                <div className={`flex justify-around px-1  xs:space-x-6 xs:items-center xs:justify-center absolute w-full text-sm font-medium md:text-[15px] ${isSearch ? 'flex top-23 md:bottom-4' : 'bottom-4 md:hidden'}`}>
+                        <Link className="text-700 hover:text-nav-hover " >bouquets</Link>
+                        <Link className="text-700 hover:text-nav-hover " >flowers</Link>
+                        <Link className="text-700 hover:text-nav-hover " >occassions</Link>
+                        <Link className="text-700 hover:text-nav-hover " >colors</Link>
+                        <Link className="text-700 hover:text-nav-hover " >popular</Link>
                     </div>
             </div>
         
--- frontend/src/components/common/searchBar.jsx
+++ frontend/src/components/common/searchBar.jsx
@@ -13,22 +13,21 @@ const SearchBar = ({isSearch, setIsSearch}) => {
 
     return (
        
-        <div className="flex items-center w-full justify-around" >
+        <div className="searchContainer flex items-center w-full justify-around" >
             {isSearch ? (
-                <form className="flex items-center">
-                    <div className="flex items-center">
-                        <input className="bg-gray-100 py-2 pl-2 pr-12 rounded-lg focus:outline-none placeholder:text-gray-700" type="text" placeholder="search" value={searchQuery} onChange={(value)=>{setSearchQuery(value)}} />
+                <form className="searchForm flex items-center w-full pl-4 pr-4 md:w-3/4 justify-between">
+                    <div className="flex items-center w-full  bg-gray-100 rounded-lg focus:outline-none px-2">
+                        <input className="w-full py-2 pl-2 pr-12 rounded-lg focus:outline-none placeholder:text-gray-700" type="text" placeholder="search" value={searchQuery} onChange={(value)=>{setSearchQuery(value)}} />
 
                         <button type="submit" onClick={()=> handleSearch()} className="ml-2 text-black hover:text-gray-700">
                             <ImSearch className="h-5 w-5"/>
                         </button>
                     </div>
-                    <div className='flex items-center'>
+                    <div className={`flex items-center ${isSearch ? 'md:hidden' : ''}`}>
                         <button onClick={()=> setIsSearch(false)} className="ml-5 text-black hover:text-gray-700">
                             <HiMiniXMark className="h-6 w-6"/>
                         </button>
                     </div>
-                    
                 </form>
                
             )  : (
